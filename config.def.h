d4c115f7a5e5791542946ec6e68fa7499913fe0c
Nelson Ripoll <contact@nelsonripoll.com>
2022-03-09 13:31:37 -0600

    update font; update color theme to dracula

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Droid Sans Mono:style=Regular:size=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -95,33 +95,29 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+  	/* 8 normal colors */
+  	[0] = "#000000", /* black   */
+  	[1] = "#ff5555", /* red     */
+  	[2] = "#50fa7b", /* green   */
+  	[3] = "#f1fa8c", /* yellow  */
+  	[4] = "#bd93f9", /* blue    */
+  	[5] = "#ff79c6", /* magenta */
+  	[6] = "#8be9fd", /* cyan    */
+  	[7] = "#bfbfbf", /* white   */
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+  	/* 8 bright colors */
+  	[8]  = "#4d4d4d", /* black   */
+  	[9]  = "#ff6e67", /* red     */
+  	[10] = "#5af78e", /* green   */
+  	[11] = "#f4f99d", /* yellow  */
+  	[12] = "#caa9fa", /* blue    */
+  	[13] = "#ff92d0", /* magenta */
+  	[14] = "#9aedfe", /* cyan    */
+  	[15] = "#e6e6e6", /* white   */
 
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+  	/* special colors */
+  	[256] = "#282a36", /* background */
+  	[257] = "#f8f8f2", /* foreground */
 };
 
 
@@ -129,10 +125,18 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg  = 257;
+unsigned int defaultbg  = 256;
+unsigned int defaultcs  = 257;
+unsigned int defaultrcs = 256;
+
+/*
+ * Colors used, when the specific fg == defaultfg. So in reverse mode this
+ * will reverse too. Another logic would only make the simple feature too
+ * complex.
+ */
+static int defaultitalic = 7;
+static int defaultunderline = 7;
 
 /*
  * Default shape of cursor
